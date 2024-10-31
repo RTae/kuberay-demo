262563139d4ac9f7fb57ddbb20c99c98b21369bd
Natthanan Bhukan <natthanan.bhu@cjexpress.co.th>
2024-10-31 15:23:26 +0700

    fix : wrong config

--- crd-demo/sample/vllm.py
+++ crd-demo/sample/vllm.py
@@ -18,7 +18,7 @@ from vllm.entrypoints.openai.protocol import (
     ErrorResponse,
 )
 from vllm.entrypoints.openai.serving_chat import OpenAIServingChat
-from vllm.entrypoints.openai.serving_engine import LoRAModulePath
+from vllm.entrypoints.openai.serving_engine import (LoRAModulePath, BaseModelPath)
 from vllm.utils import FlexibleArgumentParser
 
 logger = logging.getLogger("ray.serve")
@@ -56,9 +56,9 @@ class VLLMDeployment:
         if not self.openai_serving_chat:
             model_config = await self.engine.get_model_config()
             # Determine the name of the served model for the OpenAI client.
-            served_model_names = [{
-                "name": self.engine_args.model
-            }]
+            served_model_names = [
+                BaseModelPath(name=self.engine_args.model, model_path=self.engine_args.model)
+            ]
             self.openai_serving_chat = OpenAIServingChat(
                 self.engine,
                 model_config,
